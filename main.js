7a53f20b404e44f4d13f2bb5eaebedca3b0dac51
JeffreyCroatoan23 <omnia2332@gmail.com>
2019-12-10 21:36:36 -0600

    cambio desde Nacer

---
+++ main.js
@@ -0,0 +1,60 @@
+
+/*_______________Onboarding______________________*/
+/*
+tutorial mariachi
+*/
+
+
+/* _______Pantalla de Inicio________*/
+
+/*
+
+
+*/
+
+/*
+tenemos una pantalla con el logo animado 
+interacción con sonido - btn - iniciar y diga: Yiiija!
+*/
+
+/* __________________Formas______________________*/
+
+/*______________Integrantes______________________*/
+
+/*
+3
+5
+10
+20
+*/
+
+/*_____________________Tiempo____________________*/
+
+/*
+30 min - "casual"
+1 hrs - Para  
+2 hrs - Para pachangones
+*/
+
+
+/**/
+
+/*_________________Distancia mapa________________*/
+
+/*
+¿Programacion de mariachi? ¿Cuando?
+Sugerencia - canciones - libre o repertorio 
+donde quieres que llegue tu mariachi
+tiempo
+*/
+
+/*
+
+*/
+
+
+let playSound(soundfile) {
+document.getElementById("dummy").innerHTML= "<embed src=\""
++soundfile+"\" hidden=\"true\" autostart=\"true\"
+loop=\"false\" />";
+}
